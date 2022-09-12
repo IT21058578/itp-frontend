c32e3bb21ef38a69a7173eec60d2ea971f9fe6ac
IT21008450 <87602998+IT21008450@users.noreply.github.com>
2022-09-12 20:25:18 +0530

    update App.js by adding SC parts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { AdminLayout, EmpLayout } from "./layouts"
+import { AdminLayout, EmpLayout } from "./layouts";
+import Service from './pages/Service';
+import ScAdminPanel from './pages/ScAdminPanel';
+import ScPopupUpdate from './components/ScPopupUpdate';
+import CreatForm from './components/ScCreateForm';
 
 //Place Main routes here.
 function App() {
@@ -8,6 +12,10 @@ function App() {
       <Routes>
         <Route path="employee/*" element={<EmpLayout />}></Route>
         <Route path="admin/*" element={<AdminLayout />} ></Route>
+        <Route path='/' element={<Service />} />
+        <Route path='/admin' element={<ScAdminPanel />} />
+        <Route path='/update' element={<ScPopupUpdate />} />
+        <Route path='/create' element={<CreatForm />} />
       </Routes>
     </Router>
   );
