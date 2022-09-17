38a8089ccd9474183e1247d29f970a087f37f918
IT21008450 <87602998+IT21008450@users.noreply.github.com>
2022-09-17 23:02:40 +0530

    updated

--- src/components/ScCreateForm.jsx
+++ src/components/ScCreateForm.jsx
@@ -11,36 +11,21 @@ function CreatForm() {
       navigate('/admin');
   }
 
-  const[id,setId] =useState();
+
   const[name,setName] =useState();
   const[description,setDescription] =useState();
 
-  const [idArray, setIdArray]= useState([]);
-  function checkId(sid){
-    axios.get("http://localhost:8080/findAllCategories")
-    .then(response => response.data)
-    .then((err) => err);
-
-    //console.log("service :",this.res);
-
-    //console.log(service);
-    //const numbers = [1, 2, 3, 4, 5];
-    //const doubled = numbers.map((number) => number );
-    //console.log(doubled);
-    //const ids = [];
-    //response.data = ids;
-  }
-  checkId();
+ 
 
   function create(){
     axios.post("http://localhost:8080/SC",
     {
-        id: 99,
-        name: "cteate",
-        description: "create"
+        name: name,
+        image : "https://via.placeholder.com/400",
+        description: description
     })
     .then(response => response.data)
-    .catch((err) => console.log(err));
+    .then(alert("The service created Successfully!"));
   }
 
   return (
@@ -54,7 +39,7 @@ function CreatForm() {
             <label class="block text-sm font-medium text-gray-500 dark:text-gray-400" for="inline-full-name">
                  Service Name  
               </label>
-              <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="updatedName" type="text" placeholder="name" onChange={(e)=>{checkId(e.target.value)}}/><br/>
+              <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="updatedName" type="text" placeholder="name" onChange={2}/><br/>
               <br/>
 
               <label class="block text-sm font-medium text-gray-500 dark:text-gray-400" for="inline-full-name">
@@ -72,11 +57,11 @@ function CreatForm() {
               <label for="inline-full-name" className="block mb-2 text-sm font-medium text-gray-500 dark:text-gray-400" >
                  Description  
               </label>
-              <textarea rows="4" cols="50" className="p-2.5 text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="updatedDesc" placeholder="Description" onChange={2}></textarea>
+              <textarea rows="4" cols="50" className="p-2.5 text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="updatedDesc" placeholder="Description" onChange={(e)=>{setDescription(e.target.value)}}></textarea>
 
                 <button className="absolute top-4 right-4 box-border" onClick={() => {navigateToAdminPanel();}}>X</button>
                 <br/>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>{create();}}>SUBMIT</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>{create();navigateToAdminPanel();}}>Create New Service</button>
                 
             </div>
             </form>
