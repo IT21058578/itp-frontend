fcd4ca04bc0f7c2d40e346df3e4e104def70b633
IT21008450 <87602998+IT21008450@users.noreply.github.com>
2022-09-24 21:34:05 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Service from './pages/Service';
 import ScAdminPanel from './pages/ScAdminPanel';
 import ScPopupUpdate from './components/ScPopupUpdate';
 import CreatForm from './components/ScCreateForm';
+import ScHcBooking from './pages/ScHcBooking';
+import ScBookingHook from './hooks/ScBookingHook';
+import ScLcBooking from './pages/ScLcBooking';
+import ServiceRequestForm from './components/ServiceRequestForm';
 
 //Place Main routes here.
 function App() {
@@ -12,10 +16,14 @@ function App() {
       <Routes>
         <Route path="employee/*" element={<EmpLayout />}></Route>
         <Route path="admin/*" element={<AdminLayout />} ></Route>
-        <Route path='/' element={<Service />} />
+        <Route exact path='/service' element={<Service />} />
         <Route path='/admin' element={<ScAdminPanel />} />
         <Route path='/update' element={<ScPopupUpdate />} />
         <Route path='/create' element={<CreatForm />} />
+        <Route path='/homeClean' element={<ScHcBooking />} />
+        <Route path='/booking' element={<ScBookingHook />} />
+        <Route path='/laundryClean' element={<ScLcBooking />} />
+        <Route path='/req' element={<ServiceRequestForm />} />
       </Routes>
     </Router>
   );
