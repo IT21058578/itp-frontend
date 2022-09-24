af183db009d0dcc91d7143bc8ac5a7b02a8596ca
IT21008450 <87602998+IT21008450@users.noreply.github.com>
2022-09-24 21:35:31 +0530

    added comments

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ function App() {
         <Route path="employee/*" element={<EmpLayout />}></Route>
         <Route path="admin/*" element={<AdminLayout />} ></Route>
         <Route exact path='/service' element={<Service />} />
-        <Route path='/admin' element={<ScAdminPanel />} />
-        <Route path='/update' element={<ScPopupUpdate />} />
-        <Route path='/create' element={<CreatForm />} />
+        <Route path='/admin' element={<ScAdminPanel />} />//admin 
+        <Route path='/update' element={<ScPopupUpdate />} />//admin update service
+        <Route path='/create' element={<CreatForm />} />//admin create service
         <Route path='/homeClean' element={<ScHcBooking />} />
         <Route path='/booking' element={<ScBookingHook />} />
         <Route path='/laundryClean' element={<ScLcBooking />} />
