abd6070a56461f506c57136124ff3287592d7af2
Gayan Kalinga Bulathsinhala <bg15407@gmail.com>
2022-09-25 14:12:13 +0530

    finished data retrieving to invice

--- src/pages/invoice/invoiceCustomer.js
+++ src/pages/invoice/invoiceCustomer.js
@@ -1,9 +1,30 @@
+import { Card } from "flowbite-react";
 
-function InvoiceCustomer({id, firstName, lastName}){
+
+function InvoiceCustomer({
+  id, firstName, lastName, address, email,
+  invoiceDate, expireDate
+
+}){
   return(
-    <div>
-      <h1>{firstName}</h1>
-      <p>{lastName}</p>
+    <div className="flex-auto grid gap-4 grid-cols-2">
+    
+    <div className="flex-auto"> 
+    <Card>
+      <h2>Bill To</h2>
+      <h1>{firstName + " " + lastName}</h1>
+      <p>{address}</p>
+      <p>{email}</p>
+    </Card>
+    </div>
+
+    <div className="flex-auto"> 
+    <Card>
+      <h1>Invoice No: {id}</h1>
+      <p>Invoice Date: {invoiceDate}</p>
+      <p>Due Date: {expireDate}</p>
+    </Card>
+    </div>
     </div>
   )
 }
