61aa08dd8c0862966f980fd23417fba47bfcb7d7
IT21008450 <87602998+IT21008450@users.noreply.github.com>
2022-09-28 10:19:44 +0530

    updated
    remove the "exact" command

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         <Route path="employee/*" element={<EmpLayout />}></Route>
         <Route path="admin/*" element={<AdminLayout />} ></Route>
         <Route path="invoice/*" element={<Invoice />} ></Route>
-        <Route exact path='/service' element={<Service />} />
+        <Route path='/service' element={<Service />} />
          <Route path='/admin' element={<ScAdminPanel />} />
          <Route path='/update' element={<ScPopupUpdate />} />
          <Route path='/create' element={<CreatForm />} />
