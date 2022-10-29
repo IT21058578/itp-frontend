bed97ecf15800ea037bebec5d1b7c8042717f578
IT21150098 <IT21150098@my.sliit.lk>
2022-10-29 10:13:23 +0530

    test 01

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:8080/api/",
-
+  // baseURL: "http://localhost:8080/api/",
+// 
   withCredentials: false,
   headers: {
     "Access-Control-Allow-Origin": "*",
