2f4152037a19e273a1053ac0781706405fbfc969
IT21008450 <it21008450@my.sliit.lk>
2022-11-01 13:24:12 +0530

    ui and service addtoCart btn

--- src/pages/invoice/PaymentInstrument.js
+++ src/pages/invoice/PaymentInstrument.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from "react";
 import { useForm } from "react-hook-form";
 
-function PaymentInstrument({email, total}) {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+function PaymentInstrument({ email, total }) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
 
-  return(
-    <div>
-
-    </div>
-  );
-
-
+  return <div></div>;
 }
 
 export default PaymentInstrument;
-
