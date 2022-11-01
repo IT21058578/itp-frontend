fb4a9f439590e30ae6f19054e5a6fa3810dcb7bf
Gayan Kalinga Bulathsinhala <bg15407@gmail.com>
2022-11-01 22:14:29 +0530

    fixed the user profile layout & table not fitting to the layout

--- src/components/Payment/Instruments.js
+++ src/components/Payment/Instruments.js
@@ -8,41 +8,43 @@ function Instruments({ cards }) {
   }, [cards]);
 
   return (
-    <Table>
-      <Table.Head>
-        <Table.HeadCell>Card Number</Table.HeadCell>
-        <Table.HeadCell>Expiry Date</Table.HeadCell>
-        <Table.HeadCell>Category</Table.HeadCell>
-        <Table.HeadCell>Type</Table.HeadCell>
-        <Table.HeadCell>
-          <span className="sr-only">Edit</span>
-        </Table.HeadCell>
-      </Table.Head>
-      <Table.Body className="divide-y">
-        {cards == null
-          ? ""
-          : cards?.map((card) => (
-              <Table.Row
-                className="bg-white dark:border-gray-700 dark:bg-gray-800"
-                key={card?.id}
-              >
-                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                  {card?.cardNumber}
-                </Table.Cell>
-                <Table.Cell>{card?.expiryDate}</Table.Cell>
-                <Table.Cell>{card?.cardType}</Table.Cell>
-                <Table.Cell>
-                  <a
-                    href="/tables"
-                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                  >
-                    Edit
-                  </a>
-                </Table.Cell>
-              </Table.Row>
-            ))}
-      </Table.Body>
-    </Table>
+    <div className="flex flex-col mr-16 h-full">
+      <Table>
+        <Table.Head>
+          <Table.HeadCell>Card Number</Table.HeadCell>
+          <Table.HeadCell>Expiry Date</Table.HeadCell>
+          <Table.HeadCell>Category</Table.HeadCell>
+          <Table.HeadCell>Type</Table.HeadCell>
+          <Table.HeadCell>
+            <span className="sr-only">Edit</span>
+          </Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          {cards == null
+            ? ""
+            : cards?.map((card) => (
+                <Table.Row
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                  key={card?.id}
+                >
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    {card?.cardNumber}
+                  </Table.Cell>
+                  <Table.Cell>{card?.expiryDate}</Table.Cell>
+                  <Table.Cell>{card?.cardType}</Table.Cell>
+                  <Table.Cell>
+                    <a
+                      href="/tables"
+                      className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                    >
+                      Edit
+                    </a>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+        </Table.Body>
+      </Table>
+    </div>
   );
 }
 
--- src/pages/invoice/InvoiceList.js
+++ src/pages/invoice/InvoiceList.js
@@ -18,55 +18,57 @@ function InvoiceList() {
   }, []);
 
   return (
-    <Table>
-      <Table.Head>
-        <Table.HeadCell>First Name</Table.HeadCell>
-        <Table.HeadCell>Expire Date</Table.HeadCell>
-        <Table.HeadCell>Total</Table.HeadCell>
-        <Table.HeadCell>Status</Table.HeadCell>
-        <Table.HeadCell>
-          <span className="sr-only">More</span>
-        </Table.HeadCell>
-        <Table.HeadCell>
-          <span className="sr-only">Edit</span>
-        </Table.HeadCell>
-      </Table.Head>
-      <Table.Body className="divide-y">
-        {invoices == null
-          ? ""
-          : invoices?.map((invoice) => (
-              <Table.Row
-                className="bg-white dark:border-gray-700 dark:bg-gray-800"
-                key={invoice?.id}
-              >
-                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                  {invoice?.firstName}
-                </Table.Cell>
-                <Table.Cell>{invoice?.invoiceExpireDate}</Table.Cell>
-                <Table.Cell>{invoice?.invoiceTotal}</Table.Cell>
-                <Table.Cell>
-                  {invoice?.paymentStatus ? "Paid" : "Due"}
-                </Table.Cell>
-                <Table.Cell>
-                  <a
-                    href="/tables"
-                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                  >
-                    More
-                  </a>
-                </Table.Cell>
-                <Table.Cell>
-                  <a
-                    href="/tables"
-                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                  >
-                    Edit
-                  </a>
-                </Table.Cell>
-              </Table.Row>
-            ))}
-      </Table.Body>
-    </Table>
+    <div className="flex flex-col mr-16 h-full">
+      <Table>
+        <Table.Head>
+          <Table.HeadCell>First Name</Table.HeadCell>
+          <Table.HeadCell>Expire Date</Table.HeadCell>
+          <Table.HeadCell>Total</Table.HeadCell>
+          <Table.HeadCell>Status</Table.HeadCell>
+          <Table.HeadCell>
+            <span className="sr-only">More</span>
+          </Table.HeadCell>
+          <Table.HeadCell>
+            <span className="sr-only">Edit</span>
+          </Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          {invoices == null
+            ? ""
+            : invoices?.map((invoice) => (
+                <Table.Row
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                  key={invoice?.id}
+                >
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    {invoice?.firstName}
+                  </Table.Cell>
+                  <Table.Cell>{invoice?.invoiceExpireDate}</Table.Cell>
+                  <Table.Cell>{invoice?.invoiceTotal}</Table.Cell>
+                  <Table.Cell>
+                    {invoice?.paymentStatus ? "Paid" : "Due"}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <a
+                      href="/tables"
+                      className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                    >
+                      More
+                    </a>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <a
+                      href="/tables"
+                      className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                    >
+                      {invoice?.paymentStatus ? "" : "Pay"}
+                    </a>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+        </Table.Body>
+      </Table>
+    </div>
   );
 }
 
